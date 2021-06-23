fd778a10719ca20c9b535d72be67768acb1996b1
chuong tran <chuongtrancltv@outlook.com.vn>
2021-06-23 22:28:16 +0700

    Add project files.

---
+++ oop14/oop14.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+class Animal {
+public:
+	virtual void sound() {
+		cout << "some sound";
+	}
+};
+class cat :public Animal {
+public:
+	void sound() {
+		cout << "meow meow" << endl;
+	}
+};
+class dog :public Animal {
+public:
+	void sound() {
+		cout << "gau gau" << endl;;
+	}
+};
+int main() {
+	Animal* arr[2];
+	arr[0] = new cat;
+	arr[1] = new dog;
+	for (int i = 0; i <2; i++) {
+		arr[i]->sound();
+	}
+}
